1e6f4f63be249c9241d7f5c2ce1a3b42b026dbc9
Greg1928 <gregoriosortino@protonmail.com>
2022-05-17 17:19:06 +0200

    added mileston 3

--- js/data.js
+++ js/data.js
@@ -114,13 +114,56 @@ const icons = [
 ];
 
 const icon = document.querySelector(".icons");
+let animal = [];
+let vegetable = [];
+let user = [];
 
-for (let i = 0; i < icons.length; i++) {
-	const iconsList = 
-	`
-		<i class="${icons[i].prefix}solid ${icons[i].prefix}${icons[i].name}" style="color: ${icons[i].color};"</i>
-		<p class="small">${icons[i].name}</p>
 
-	`;
-	icon.innerHTML += iconsList;
-}
+let select = document.querySelector("#form");
+select.addEventListener('change', function(){
+	if ( form.value === "1"){
+		for (let i = 0; i < icons.length; i++) {
+			const iconsList = 
+			`
+				<i class="${icons[i].prefix}solid ${icons[i].prefix}${icons[i].name}" style="color: ${icons[i].color};"</i>
+				<p class="small">${icons[i].name}</p>
+		
+			`;
+			icon.innerHTML += iconsList;
+		}
+	}else if ( form.value === "2"){
+		animal = icons.filter(icons => icons.type === "animal");
+		for (let i = 0; i < animal.length; i++) {
+			const iconsList = 
+			`
+				<i class="${animal[i].prefix}solid ${animal[i].prefix}${animal[i].name}" style="color: ${animal[i].color};"</i>
+				<p class="small">${animal[i].name}</p>
+		
+			`;
+			icon.innerHTML += iconsList;
+		}
+	}else if ( form.value === "3"){
+		vegetable = icons.filter(icons => icons.type === "vegetable");
+		for (let i = 0; i < vegetable.length; i++) {
+			const iconsList = 
+			`
+				<i class="${vegetable[i].prefix}solid ${vegetable[i].prefix}${vegetable[i].name}" style="color: ${vegetable[i].color};"</i>
+				<p class="small">${vegetable[i].name}</p>
+		
+			`;
+			icon.innerHTML += iconsList;
+		}
+	}else if ( form.value === "4"){
+		user = icons.filter(icons => icons.type === "user");
+		for (let i = 0; i < user.length; i++) {
+			const iconsList = 
+			`
+				<i class="${user[i].prefix}solid ${user[i].prefix}${user[i].name}" style="color: ${user[i].color};"</i>
+				<p class="small">${user[i].name}</p>
+		
+			`;
+			icon.innerHTML += iconsList;
+		}
+	}
+});
+
